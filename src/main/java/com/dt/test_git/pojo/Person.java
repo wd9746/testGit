69cee172a43da4763682654335f96dc9616b5bb1
wd9746 <wd974655958@163.com>
2020-06-13 16:32:42 +0800

    create git test

---
+++ src/main/java/com/dt/test_git/pojo/Person.java
@@ -0,0 +1,13 @@
+package com.dt.test_git.pojo;
+
+/**
+ * @ProjectName: test_git
+ * @ClassName: Person
+ * @Description: TODO(一句话描述该类的功能)
+ * @Author: WD
+ * @Date: 2020/6/13 16:31
+ */
+public class Person {
+
+    private String name;//姓名
+}
